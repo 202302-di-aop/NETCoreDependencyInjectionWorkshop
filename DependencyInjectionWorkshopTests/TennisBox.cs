36a66517ead8c51ad59647c88cec1ca858c8ba38
joeychen <joeychen@odd-e.com>
2025-02-27 16:51:53 +0800

    extract class: AllState

--- DependencyInjectionWorkshopTests/TennisBox.cs
+++ DependencyInjectionWorkshopTests/TennisBox.cs
@@ -1,9 +1,24 @@
 ﻿namespace DependencyInjectionWorkshopTests;
 
-public class TennisBox
+public class AllState
 {
     public string Score()
     {
         return "love all";
+    }
+}
+
+public class TennisBox
+{
+    private AllState _currentState;
+
+    public TennisBox()
+    {
+        _currentState = new AllState();
+    }
+
+    public string Score()
+    {
+        return _currentState.Score();
     }
 }
