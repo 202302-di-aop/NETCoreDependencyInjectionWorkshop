669dfa314c62891864e58da8dbfab7acf07371b4
joeychen <joeychen@odd-e.com>
2025-02-27 15:47:39 +0800

    interceptor for log parameters

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -45,6 +45,18 @@
 
     public class FailedTooManyTimesException : Exception
     {
+        public FailedTooManyTimesException()
+        {
+        }
+
+        public FailedTooManyTimesException(string message) : base(message)
+        {
+        }
+
+        public FailedTooManyTimesException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
         public string Account { get; set; }
     }
 }
--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,7 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
 using DependencyInjectionWorkshop.Models;
+
+public class LogInterceptor : IInterceptor
+{
+    private readonly IMyLogger _logger;
+
+    public LogInterceptor(IMyLogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void Intercept(IInvocation invocation)
+    {
+        var signatureContent = $"log by interceptor:{invocation.TargetType.FullName}.{invocation.Method.Name}():" +
+                               $"{string.Join("-", (invocation.Arguments.Select(x => (x ?? "").ToString())))}";
+
+        _logger.Info(signatureContent);
+
+        invocation.Proceed();
+    }
+}
+
+public class Period
+{
+    public DateTime Start { get; }
+    public DateTime End { get; }
+
+    public Period NextPeriod { get; set; }
+
+    public Period(DateTime start, DateTime end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Start)}: {Start}, {nameof(End)}: {End}, {nameof(NextPeriod)}: {NextPeriod}";
+    }
+}
 
 internal class Program
 {
@@ -13,10 +54,11 @@ internal class Program
 
         var authentication = _container.Resolve<IAuthentication>();
 
-        var isValid = await authentication.IsValid("joey", "abc", "123456");
+        var isValid = await authentication.IsValid("joey", "abc", "666666");
         // var isValid = await authentication.IsValid("joey", "abc", "wrong otp");
-        Console.WriteLine($"result:{isValid}");
+        Console.WriteLine($"joey result:{isValid}");
     }
+
 
     private static void RegisterContainer()
     {
@@ -25,10 +67,21 @@ internal class Program
         builder.RegisterType<FakeOtp>().As<IOtpProxy>();
         builder.RegisterType<FakeHash>().As<IHash>();
         // builder.RegisterType<FakeLogger>().As<ILogger>();
+        builder.RegisterType<FakeLogger>().As<IMyLogger>();
         // builder.RegisterType<FakeFailedCounter>().As<IFailedCounter>();
         // builder.RegisterType<FakeSlack>().As<INotification>();
         //
-        builder.RegisterType<AuthenticationService>().As<IAuthentication>();
+        // builder.RegisterType<FeatureToggleDispatcher>().As<IAuthentication>();
+        // builder.RegisterType<JoeyToggleManager>().As<IFeatureToggleManager>();
+        // builder.RegisterType<OAuth>();
+        // builder.RegisterType<OAuth>().As<IAuthentication>();
+        // builder.RegisterType<AuthenticationService>();
+
+        builder.RegisterType<LogInterceptor>();
+
+        builder.RegisterType<AuthenticationService>().As<IAuthentication>()
+            .EnableInterfaceInterceptors()
+            .InterceptedBy(typeof(LogInterceptor));
         //
         // builder.RegisterDecorator<FailCounterDecorator, IAuthentication>();
         // builder.RegisterDecorator<LogDecorator, IAuthentication>();
@@ -38,7 +91,77 @@ internal class Program
     }
 }
 
-internal class FakeHash: IHash
+internal class FakeLogger : IMyLogger
+{
+    public void Info(string message)
+    {
+        Console.WriteLine($"by joey: {message}");
+    }
+}
+
+internal class JoeyToggleManager : IFeatureToggleManager
+{
+    public bool IsEnableOAuth()
+    {
+        var key = Console.ReadLine();
+        return key == "T";
+    }
+}
+
+internal class FeatureToggleDispatcher : IAuthentication
+{
+    private readonly IFeatureToggleManager _featureToggleManager;
+    private readonly OAuth _newFlow;
+    private readonly AuthenticationService _oldFlow;
+
+    public FeatureToggleDispatcher(OAuth newFlow, AuthenticationService oldFlow,
+        IFeatureToggleManager featureToggleManager)
+    {
+        _newFlow = newFlow;
+        _oldFlow = oldFlow;
+        _featureToggleManager = featureToggleManager;
+    }
+
+    public Task<bool> IsValid(string account, string password, string otp)
+    {
+        if (_featureToggleManager.IsEnableOAuth())
+        {
+            return _newFlow.IsValid(account, password, otp);
+        }
+
+        return _oldFlow.IsValid(account, password, otp);
+    }
+}
+
+internal interface IFeatureToggleManager
+{
+    bool IsEnableOAuth();
+}
+
+internal class OAuth : IAuthentication
+{
+    public Task<bool> IsValid(string account, string password, string otp)
+    {
+        Console.WriteLine("I am OAuth;");
+        if (otp == "666666")
+        {
+            return Task.FromResult(true);
+        }
+
+        return Task.FromResult(false);
+    }
+}
+
+internal class FakeSlack : INotification
+
+{
+    public void Notify(string account)
+    {
+        Console.WriteLine($" fake slack - account :{account}");
+    }
+}
+
+internal class FakeHash : IHash
 {
     public string GetHashResult(string password)
     {
@@ -46,7 +169,7 @@ internal class FakeHash: IHash
     }
 }
 
-internal class FakeOtp: IOtpProxy
+internal class FakeOtp : IOtpProxy
 {
     public Task<string> GetCurrentOtp(string account)
     {
@@ -54,7 +177,7 @@ internal class FakeOtp: IOtpProxy
     }
 }
 
-internal class FakeProfileRepo:IProfileRepo
+internal class FakeProfileRepo : IProfileRepo
 {
     public string GetPasswordFromDb(string account)
     {
